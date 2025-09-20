b7970973955c55b7369697f7ee615583dc9c1a1f
magicalbose <peehana.bose@gmail.com>
2025-09-19 21:29:44 -0500

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,14 @@
+first_name = "Codingal "
+last_name = "Educations"
+
+full_name = first_name+last_name
+print("Full Name:", full_name)
+          
+word = "Coding"
+
+print("Length of String:", len(word))
+print("First Letter of String:", word[0])
+print("Last Letter of String:", word[5])
+
+print()
+print("String Sliced:", word[3:6])
