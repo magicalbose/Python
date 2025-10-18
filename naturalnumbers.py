2142a9e704b4b1930a31092c84e30d743ea2749a
magicalbose <peehana.bose@gmail.com>
2025-10-17 19:20:29 -0500

    Add files via upload

---
+++ naturalnumbers.py
@@ -0,0 +1,8 @@
+num = int(input("Enter number to check if it's a natural number: "))
+print("Number to be checked:", num)
+
+if num>0:
+    print("This is a natural number.")
+
+else:
+    print("This is not a natural number.")
