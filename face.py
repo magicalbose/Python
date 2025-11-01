967fc68773dacb0d5cbb784a0f4e174583568903
magicalbose <peehana.bose@gmail.com>
2025-10-31 21:12:24 -0500

    Add files via upload

---
+++ face.py
@@ -0,0 +1,95 @@
+from turtle import *
+
+
+class Face:
+
+    def __init__(self, xpos, ypos):
+        self.size = 90
+        self.coord = (xpos, ypos)
+        self.noseSize = 'small'
+
+    def setSize(self, radius):
+        self.size = radius
+        
+    def draw(self):
+        self.goHome()
+        pensize(3)
+        speed(0)
+        self.drawOutline()
+        self.drawEye(135)
+        self.drawEye(45)
+        self.drawMouth()
+        self.drawNose()
+        pensize(5)
+
+# --------------------------------------------------
+#    Functions that are called from with the class
+# --------------------------------------------------
+
+
+    # After drawing each part, turtle position 
+    # returns to centre. Parts can be drawn in any order
+    
+    def goHome(self):
+        penup()
+        goto(self.coord)
+        
+        setheading(0)
+        
+
+
+
+    def drawOutline(self):
+        penup()
+        # move turtle pen in forward direction 
+        forward(self.size)
+        left(90)
+        # draw a circle of given radius
+        pendown()
+        circle(self.size)
+        # return back to centre
+        self.goHome()
+
+
+
+    def drawEye(self, turn):
+        penup()      
+        left(turn)       
+        forward(self.size / 2)
+        pendown()        
+        dot(self.size/10)       
+        self.goHome()
+        
+    def drawMouth(self):
+        penup()        
+        right(135)
+        forward(self.size/1.7)
+        left(90)
+        pendown()
+        circle(self.size/1.7,90)
+        self.goHome()
+
+
+
+
+    def drawNose(self):
+        
+        if self.noseSize == 'large' :
+            dot(self.size/2, "grey")
+        elif self.noseSize == 'small' :
+            dot(self.size/6, "grey")
+        else :
+            dot(self.size/4, "grey")
+            
+        self.goHome()
+        
+
+#  start of drawing code
+#  ---------------------
+  
+f1 = Face(0, 0)
+f1.draw()
+
+showturtle()
+done()
+
