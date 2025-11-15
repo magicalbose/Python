9af1d8626de706df5de16c4e660b4452df04375e
magicalbose <peehana.bose@gmail.com>
2025-11-14 19:49:25 -0600

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,13 @@
+print("Right Angled Triangle Pattern \n")
+
+for i in range(1,6):
+    for j in range(i):
+        print("*", end = "")
+    print('\n')
+
+print("Inverted Right Angled Triangle Pattern \n")
+
+for i in range(6, 1, -1): 
+    for j in range(i, 1, -1):
+        print("*", end = "")
+    print('\n')
