06f735c63c2cf51022369a1d6e4aceb3e0f60f6d
magicalbose <peehana.bose@gmail.com>
2025-11-14 20:04:37 -0600

    Add files via upload

---
+++ denominations.py
@@ -0,0 +1,13 @@
+def numberofnotes(a):
+
+    Q = [2000, 500, 200, 100, 50, 20, 10]
+    x = 0
+
+    for i in range(7):
+        q = Q[i]
+        x = a // q
+        print("Notes of {} = {}".format(q, x))
+        a = a % q
+
+amount = int(input("Enter total amount: "))
+numberofnotes(amount)
